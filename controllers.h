0d7155366422e0c0dac682b2ed373130858053b7
Gabriel <ccgabriel.888@gmail.com>
2025-09-04 18:16:00 -0700

    Frontend-Client-Side and Backend-server-side

---
+++ controllers.h
@@ -0,0 +1,13 @@
+//
+// Created by gabri on 9/4/2025.
+//
+
+#ifndef AUCTION_SYSTEM_CONTROLLERS_H
+#define AUCTION_SYSTEM_CONTROLLERS_H
+
+
+class controllers {
+};
+
+
+#endif //AUCTION_SYSTEM_CONTROLLERS_H
---
+++ db.h
@@ -0,0 +1,13 @@
+//
+// Created by gabri on 9/4/2025.
+//
+
+#ifndef AUCTION_SYSTEM_DB_H
+#define AUCTION_SYSTEM_DB_H
+
+
+class db {
+};
+
+
+#endif //AUCTION_SYSTEM_DB_H
