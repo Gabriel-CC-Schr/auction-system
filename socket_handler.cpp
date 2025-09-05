474d8a6f98221242c284526081ecac2b2045983b
Gabriel <ccgabriel.888@gmail.com>
2025-09-04 23:46:29 -0700

    Socket

---
+++ socket_handler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gabri on 9/4/2025.
+//
+
+#include "socket_handler.h"
---
+++ socket_handler.h
@@ -0,0 +1,13 @@
+//
+// Created by gabri on 9/4/2025.
+//
+
+#ifndef AUCTION_SYSTEM_SOCKET_HANDLER_H
+#define AUCTION_SYSTEM_SOCKET_HANDLER_H
+
+
+class socket_handler {
+};
+
+
+#endif //AUCTION_SYSTEM_SOCKET_HANDLER_H
