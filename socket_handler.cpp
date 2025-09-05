3712e604030ae633f36f5467342a0735dcfa87ea
Gabriel <ccgabriel.888@gmail.com>
2025-09-05 00:33:45 -0700

    Creating a Socket

--- socket_handler.cpp
+++ socket_handler.cpp
@@ -3,3 +3,46 @@
 //
 
 #include "socket_handler.h"
+
+// Default constructor
+
+HDE::socket_handler socket::socket_handler(int domain, int service, int protocol,
+    int port, u_long interface){
+
+    // Define address structure
+    address.sin_family = domain;
+    adress.sin_port = htons(port);
+    adess.sin_address.s_addr = htonl(interface);
+    // Establish socket
+    sock = socket(domain, service, protocol);
+    test_connection(sock);
+    // Establish network connection
+    connection = connect_to_netork(sock, address);
+    test_connection(connection);
+}
+
+// Test connection virtual function
+
+void HDE::socket_handler::test_connection(int iteam_to_test){
+    // Confirms that the sockcet or connection has been properly established
+    if (item_to_test < 0){
+
+        perror("Failed to connect....");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Getter funtions
+
+struck sockaddr_in HDE::socket_handler::get_address(){
+return address;
+}
+
+int HDE::socket_handler::get_sock(){
+return sock;
+}
+
+int HDE::socket_handler::get_connection(){
+return connection;
+}
+
--- socket_handler.h
+++ socket_handler.h
@@ -5,9 +5,32 @@
 #ifndef AUCTION_SYSTEM_SOCKET_HANDLER_H
 #define AUCTION_SYSTEM_SOCKET_HANDLER_H
 
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+
+namespace HDE{
 
 class socket_handler {
+private:
+struct sockaddr_in address;
+    int sock;
+    int connection;
+public:
+    // Constructor
+    socket_handler(int domain, int service, int protocol, int port, u_long interface);
+    // Virtual function to connect to a network
+    virtual void connect_to_netork(int sock, struct sockaddr_in address) = 0;
+    // Function to test sockets and connections
+    void test_connection(int);
+    // gettter functions
+    struct sockaddr_in get_address();
+    int get_sock();
+    int get_connection();
+
 };
+}
 
 
 #endif //AUCTION_SYSTEM_SOCKET_HANDLER_H
